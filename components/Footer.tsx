a9290cdfb3b3ae60962d08faa3bf5b82fa6bc467
benisoharj-oss <benisoharj@gmail.com>
2026-04-28 23:05:10 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
     },
     {
       label: "Instagram",
-      href: "https://instagram.com/benisohafusion",
+      href: "https://www.instagram.com/benisoha",
       icon: (
         <svg viewBox="0 0 24 24" className="w-4 h-4" fill="currentColor">
           <path d="M12 2.2c3.2 0 3.6 0 4.9.1 3.3.1 4.8 1.7 4.9 4.9.1 1.3.1 1.6.1 4.8 0 3.2 0 3.6-.1 4.8-.1 3.2-1.7 4.8-4.9 4.9-1.3.1-1.6.1-4.9.1-3.2 0-3.6 0-4.8-.1-3.3-.1-4.8-1.7-4.9-4.9-.1-1.3-.1-1.6-.1-4.8 0-3.2 0-3.6.1-4.8C2.4 4 4 2.4 7.2 2.3c1.2-.1 1.6-.1 4.8-.1zm0-2.2C8.7 0 8.3 0 7.1.1 2.7.3.3 2.7.1 7.1.1 8.3 0 8.7 0 12c0 3.3 0 3.7.1 4.9.2 4.4 2.6 6.8 7 7 1.2.1 1.6.1 4.9.1 3.3 0 3.7 0 4.9-.1 4.4-.2 6.8-2.6 7-7 .1-1.2.1-1.6.1-4.9 0-3.3 0-3.7-.1-4.9C23.7 2.7 21.3.3 16.9.1 15.7 0 15.3 0 12 0zm0 5.8a6.2 6.2 0 100 12.4A6.2 6.2 0 0012 5.8zM12 16a4 4 0 110-8 4 4 0 010 8zm6.4-11.8a1.44 1.44 0 100 2.88 1.44 1.44 0 000-2.88z" />
@@ -31,7 +31,7 @@ export default function Footer() {
     },
     {
       label: "X",
-      href: "https://x.com/benisohafusion",
+      href: "https://x.com/Benisoharj",
       icon: (
         <svg viewBox="0 0 24 24" className="w-4 h-4" fill="currentColor">
           <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
@@ -40,7 +40,7 @@ export default function Footer() {
     },
     {
       label: "YouTube",
-      href: "https://youtube.com/@benisohafusion",
+      href: "https://youtube.com/@benisoha",
       icon: (
         <svg viewBox="0 0 24 24" className="w-4 h-4" fill="currentColor">
           <path d="M23.5 6.2a3 3 0 00-2.1-2.1C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.4.6a3 3 0 00-2.1 2.1C0 8.1 0 12 0 12s0 3.9.5 5.8a3 3 0 002.1 2.1c1.9.6 9.4.6 9.4.6s7.5 0 9.4-.6a3 3 0 002.1-2.1C24 15.9 24 12 24 12s0-3.9-.5-5.8zm-13.9 9.4V8.4l6.3 3.6-6.3 3.6z" />
